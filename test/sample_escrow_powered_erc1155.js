7523125391bfc2b5ecd3fbbd17a40ba905ed7feb
Luis Masuelli <luis@datagran.io>
2022-07-23 10:53:22 -0500

    Adding some test helpers.

--- test/sample_escrow_powered_erc1155.js
+++ test/sample_escrow_powered_erc1155.js
@@ -1,5 +1,12 @@
 const SampleEscrowPoweredERC1155 = artifacts.require("SampleEscrowPoweredERC1155");
 
+const {
+  BN,           // Big Number support
+  constants,    // Common constants, like the zero address and largest integers
+  expectEvent,  // Assertions for emitted events
+  expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
+
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
@@ -13,6 +20,6 @@ contract("SampleEscrowPoweredERC1155", function (/* accounts */) {
   });
 
   it("should assert true", async function () {
-    return assert.isTrue(true);
+
   });
 });
