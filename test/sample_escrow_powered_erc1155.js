a0c5e2447dcf9acce601a123ab14775bdba31b20
Luis Masuelli <luis@datagran.io>
2022-07-23 16:29:10 -0500

    Adding the remaining error tests.

--- test/sample_escrow_powered_erc1155.js
+++ test/sample_escrow_powered_erc1155.js
@@ -268,7 +268,14 @@ contract("SampleEscrowPoweredERC1155", function (accounts) {
   });
 
   it("must check data and state errors", async function() {
-    // 0. Direct usage.
+    // 0. Direct usage, and many errors.
+    console.log("0. Direct usage, and many errors");
+    await expectRevert(contract.dealStart(
+      accounts[0], constants.ZERO_ADDRESS, [], [], {from: accounts[0]}
+    ), revertReason("EscrowPoweredERC1155: transfer from/to the zero address"));
+    await expectRevert(contract.dealStart(
+        constants.ZERO_ADDRESS, accounts[1], [], [], {from: accounts[0]}
+    ), revertReason("EscrowPoweredERC1155: transfer from/to the zero address"));
     await expectRevert(contract.dealStart(
       accounts[0], accounts[1], [], [], {from: accounts[0]}
     ), revertReason("EscrowPoweredERC1155: token ids and amounts length mismatch or 0"));
@@ -286,16 +293,24 @@ contract("SampleEscrowPoweredERC1155", function (accounts) {
       {from: accounts[0]}
     );
     // 1. The balances must be unchanged, on both accounts.
+    console.log("1. The balances must be unchanged, on both accounts");
     let ids = [new BN("1"), new BN("2"), new BN("3")];
     let amounts = [new BN("500000000000000000"), new BN("1000000000000000000"), new BN("1500000000000000000")];
     await expectBalances(ids, amounts, accounts[0]);
     await expectBalances(ids, amounts, accounts[1]);
     // 2. An event must have been triggered.
+    console.log("2. An event must have been triggered");
     expectEvent(tx, "DealStarted", {
       dealId: new BN("3"), emitter: accounts[0], receiver: accounts[1]
     });
     // 3. Also, the deal must be valid.
-    let newDeal = await contract.deals(new BN("3"), {from: accounts[0]});
+    console.log("3. Also, the deal must be valid");
+    let newDeal = await contract.deals(new BN("4"), {from: accounts[0]});
+    assert.isTrue(
+      newDeal.emitter === constants.ZERO_ADDRESS,
+      "The emitter must be zero since an invalid deal was selected, but instead it is " + newDeal.emitter
+    );
+    newDeal = await contract.deals(new BN("3"), {from: accounts[0]});
     assert.isTrue(
       newDeal.emitter === accounts[0],
       "The new deal has an emitter of " + newDeal.emitter + " but the expected one is: " + accounts[0]
@@ -309,17 +324,45 @@ contract("SampleEscrowPoweredERC1155", function (accounts) {
       "The new deal must have a state of 0, not " + newDeal.state
     );
     // 4. Then, the deal must be accepted by the receiver.
+    console.log("4. Then, the deal must be accepted by the receiver");
+    await expectRevert(
+      contract.dealAccept(new BN("3"), [new BN("3")], [new BN("150000000000000000")], {from: accounts[0]}),
+      revertReason("EscrowPoweredERC1155: caller is not receiver nor approved")
+    );
+    await expectRevert(
+      contract.dealAccept(new BN("4"), [new BN("3")], [new BN("150000000000000000")], {from: accounts[1]}),
+      revertReason("EscrowPoweredERC1155: invalid deal")
+    );
+    await expectRevert(
+      contract.dealAccept(new BN("3"), [new BN("3"), new BN("1")], [new BN("150000000000000000")], {from: accounts[1]}),
+      revertReason("EscrowPoweredERC1155: token ids and amounts length mismatch or 0")
+    );
+    await expectRevert(
+      contract.dealAccept(new BN("3"), [], [], {from: accounts[1]}),
+      revertReason("EscrowPoweredERC1155: token ids and amounts length mismatch or 0")
+    );
+    await expectRevert(
+      contract.dealConfirm(new BN("3"), {from: accounts[0]}),
+      revertReason("EscrowPoweredERC1155: deal is not in just-accepted state")
+    );
+    await expectRevert(
+      contract.dealAccept(new BN("3"), [new BN("3")], [new BN("0")], {from: accounts[1]}),
+      revertReason("EscrowPoweredERC1155: token in position 0 must not have amount of 0")
+    );
     tx = await contract.dealAccept(new BN("3"), [new BN("3")], [new BN("150000000000000000")], {from: accounts[1]});
     // 5. The balances must be unchanged, on both accounts.
+    console.log("5. The balances must be unchanged, on both accounts");
     ids = [new BN("1"), new BN("2"), new BN("3")];
     amounts = [new BN("500000000000000000"), new BN("1000000000000000000"), new BN("1500000000000000000")];
     await expectBalances(ids, amounts, accounts[0]);
     await expectBalances(ids, amounts, accounts[1]);
     // 6. An event must have been triggered.
+    console.log("6. An event must have been triggered");
     expectEvent(tx, "DealAccepted", {
       dealId: new BN("3"), emitter: accounts[0], receiver: accounts[1]
     });
     // 7. Also, the deal must be valid.
+    console.log("7. Also, the deal must be valid");
     newDeal = await contract.deals(new BN("3"), {from: accounts[1]});
     assert.isTrue(
       newDeal.emitter === accounts[0],
@@ -334,18 +377,34 @@ contract("SampleEscrowPoweredERC1155", function (accounts) {
       "The new deal must have a state of 1, not " + newDeal.state
     );
     // 8. Them, the deal must be confirmed by the emitter.
+    console.log("8. Them, the deal must be confirmed by the emitter");
+    await expectRevert(
+      contract.dealConfirm(new BN("4"), {from: accounts[0]}),
+      revertReason("EscrowPoweredERC1155: invalid deal")
+    );
+    await expectRevert(
+      contract.dealConfirm(new BN("3"), {from: accounts[1]}),
+      revertReason("EscrowPoweredERC1155: caller is not emitter nor approved")
+    );
+    await expectRevert(
+      contract.dealAccept(new BN("3"), [new BN("3")], [new BN("150000000000000000")], {from: accounts[1]}),
+      revertReason("EscrowPoweredERC1155: deal is not in just-created state")
+    );
     tx = await contract.dealConfirm(new BN("3"), {from: accounts[0]});
     // 9. An event must have been triggered.
+    console.log("9. An event must have been triggered");
     expectEvent(tx, "DealConfirmed", {
       dealId: new BN("3"), emitter: accounts[0], receiver: accounts[1]
     });
     // 10. And now, the balances must be different, reflecting the exchanges.
+    console.log("10. And now, the balances must be different, reflecting the exchanges");
     ids = [new BN("1"), new BN("2"), new BN("3")];
     let amounts1 = [new BN("450000000000000000"), new BN("900000000000000000"), new BN("1650000000000000000")];
     let amounts2 = [new BN("550000000000000000"), new BN("1100000000000000000"), new BN("1350000000000000000")];
     await expectBalances(ids, amounts1, accounts[0]);
     await expectBalances(ids, amounts2, accounts[1]);
     // 11. Ok, let's reinstate their balances.
+    console.log("11. Ok, let's reinstate their balances");
     await contract.safeBatchTransferFrom(
       accounts[0], accounts[1], [new BN("3")],
       [new BN("150000000000000000")], web3.utils.asciiToHex("test"),
