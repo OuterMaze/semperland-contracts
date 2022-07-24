1033b300e05f8d0c69c87209212ddfca05aeb0a3
Luis Masuelli <luis@datagran.io>
2022-07-24 10:52:35 -0500

    Adding the NativePayable contract.

---
+++ contracts/NativePayable.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8 <0.9.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+/**
+ * A mixin for classes which have natively payable methods.
+ */
+abstract contract NativePayable {
+    /**
+     * Requires an exact payment on the underlying method.
+     */
+    modifier hasNativeTokenPrice(string memory concept, uint256 price) {
+        uint256 value = msg.value;
+        require(value == price, string(abi.encodePacked(
+            concept, " requires an exact payment of ", Strings.toString(price), " but ",
+            Strings.toString(value), " was given"
+        )));
+        _;
+    }
+}
