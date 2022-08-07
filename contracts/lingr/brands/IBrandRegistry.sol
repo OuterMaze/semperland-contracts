89785de116ea9defba8ae825e0a72e2365487694
Luis Masuelli <luis@datagran.io>
2022-08-07 09:43:41 -0500

    Adding the IBrandRegistry.

---
+++ contracts/lingr/brands/IBrandRegistry.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8 <0.9.0;
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+/**
+ * A contract satisfying this interface will have capabilities
+ * to query the existing brands' existence or metadata.
+ */
+interface IBrandRegistry is IERC165 {
+    /**
+     * Assembles the whole metadata for a brand. WARNING: This method
+     * will consume a lot of gas if invoked inside a transaction, so
+     * it is recommended to invoke this method in the context of a
+     * CALL, and never in the context of a SEND (even as part of other
+     * contract's code).
+     */
+    function brandMetadataURI(address _brandId) external view returns (string memory);
+
+    /**
+     * Tells whether an address corresponds to a registered brand or not.
+     */
+    function brandExists(address _brandId) public view returns (bool);
+}
