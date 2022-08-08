f2461ae4383b84bdb13e3944ae3e888514d2fe42
Luis Masuelli <luis@datagran.io>
2022-08-07 21:58:09 -0500

    Removing an unused library.

--- contracts/lingr/samples/SampleERC1155WithBrandRegistry.sol
+++ contracts/lingr/samples/SampleERC1155WithBrandRegistry.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "../brands/BrandRegistry.sol";
-import "../../IdUtils.sol";
 
 contract SampleERC1155WithBrandRegistry is ERC1155, BrandRegistry {
     address public owner;
