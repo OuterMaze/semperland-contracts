69e0eb9754b9a4d81f23911103a37816747fce2d
Luis Masuelli <luis@datagran.io>
2022-08-21 20:22:34 -0500

    Removing some unneeded imports.

--- contracts/lingr/plugins/base/FTMintingPlugin.sol
+++ contracts/lingr/plugins/base/FTMintingPlugin.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8 <0.9.0;
 
-import "@openzeppelin/contracts/utils/Context.sol";
 import "../../IMetaverse.sol";
-import "../../brands/IBrandRegistry.sol";
 import "./IMetaversePlugin.sol";
 
 /**
@@ -11,7 +9,7 @@ import "./IMetaversePlugin.sol";
  * or a brand. These ones are internal (typically used after testing the types
  * to be valid FT types).
  */
-abstract contract FTMintingPlugin is Context, IMetaversePlugin {
+abstract contract FTMintingPlugin is IMetaversePlugin {
     /**
      * The metaverse this plug-in belongs to. This abstract definition
      * extends it to be public (to use this one locally).
