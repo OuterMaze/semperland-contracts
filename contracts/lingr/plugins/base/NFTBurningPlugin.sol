9e51045de91290a8d65aa1a1355490eedb94df96
Luis Masuelli <luis@datagran.io>
2022-08-21 20:21:04 -0500

    Adding the NFT Minting Plugin Trait, and changing some comments in the burning trait.

--- contracts/lingr/plugins/base/NFTBurningPlugin.sol
+++ contracts/lingr/plugins/base/NFTBurningPlugin.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8 <0.9.0;
 
-import "@openzeppelin/contracts/utils/Context.sol";
 import "../../IMetaverse.sol";
-import "../../brands/IBrandRegistry.sol";
 import "./IMetaversePlugin.sol";
 
 /**
- * This trait defines utilities that burn NFT tokens, both in the system scope
- * or a brand. These ones are internal (typically used after testing the types
- * to be valid NFT types).
+ * This trait defines utilities that burn NFT tokens. These ones
+ * are internal (typically used after testing the types to be valid
+ * NFT types).
  */
-abstract contract NFTBurningPlugin is Context, IMetaversePlugin {
+abstract contract NFTBurningPlugin is IMetaversePlugin {
     /**
      * The metaverse this plug-in belongs to. This abstract definition
      * extends it to be public (to use this one locally).
@@ -19,14 +17,16 @@ abstract contract NFTBurningPlugin is Context, IMetaversePlugin {
     function metaverse() public virtual override returns (address);
 
     /**
-     * Burns a NFT (only types defined by this contract are available).
+     * Burns a NFT (only types defined by this contract should be available,
+     * unless there are good reasons to do otherwise).
      */
     function _burnNFT(uint256 _tokenId) internal {
         IMetaverse(metaverse).burnNFT(_tokenId);
     }
 
     /**
-     * Burns many NFT (only types defined by this contract are available).
+     * Burns many NFT (only types defined by this contract are available,
+     * unless there are good reasons to do otherwise).
      */
     function _burnNFTs(uint256[] memory _tokenIds) internal {
         IMetaverse(metaverse).burnNFTs(_tokenIds);
---
+++ contracts/lingr/plugins/base/NFTMintingPlugin.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8 <0.9.0;
+
+import "../../IMetaverse.sol";
+import "./IMetaversePlugin.sol";
+
+/**
+ * This trait defines utilities that mint NFT tokens. These ones are
+ * internal (typically used after testing the types to be valid NFT\
+ * types).
+ */
+abstract contract NFTMintingPlugin is IMetaversePlugin {
+    /**
+     * The metaverse this plug-in belongs to. This abstract definition
+     * extends it to be public (to use this one locally).
+     */
+    function metaverse() public virtual override returns (address);
+
+    /**
+     * Mints a NFT for a user (only types defined by this contract should
+     * be available, unless there are good reasons to do otherwise). Returns
+     * the newly minted id.
+     */
+    function _mintNFTFor(address _to, uint256 _tokenType, bytes memory _data) internal returns (uint256) {
+        return IMetaverse(metaverse).mintNFTFor(_to, _tokenType, _data);
+    }
+}
