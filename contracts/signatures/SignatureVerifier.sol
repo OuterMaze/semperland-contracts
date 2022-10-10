312e9ef3084008efbb8e72185c679a770768e381
Luis Masuelli <luis@datagran.io>
2022-10-10 11:44:24 -0500

    Making a method virtual.

--- contracts/signatures/SignatureVerifier.sol
+++ contracts/signatures/SignatureVerifier.sol
@@ -13,7 +13,7 @@ abstract contract SignatureVerifier is ISignatureVerifier {
     /**
      * A signature verifier contract satisfies the ISignatureVerifier and IERC165.
      */
-    function supportsInterface(bytes4 _interfaceId) public view returns (bool) {
+    function supportsInterface(bytes4 _interfaceId) public virtual view returns (bool) {
         return _interfaceId == type(IERC165).interfaceId || _interfaceId == type(ISignatureVerifier).interfaceId;
     }
 
